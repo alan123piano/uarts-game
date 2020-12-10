1a668ce50dbfe147bf08cc966ea0d4d8c38044c1
Alan Yang <alanyang@umich.edu>
2020-12-10 01:47:26 -0500

    WIP dialogue

---
+++ Assets/Scene1Audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene1Audio : MonoBehaviour
+{
+    public AudioClip rocketLandingSound;
+    public AudioClip rocketDoorOpenSound;
+
+    void Start()
+    {
+        PlayAudioClip(rocketLandingSound, 0);
+        PlayAudioClip(rocketDoorOpenSound, 4);
+    }
+
+    void PlayAudioClip(AudioClip clip, float delay)
+    {
+        AudioSource asrc = gameObject.AddComponent<AudioSource>();
+        asrc.clip = clip;
+        asrc.PlayDelayed(delay);
+    }
+}
