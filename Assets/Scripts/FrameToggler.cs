db3563ae32efaf30e238d46a31259a3c73acda6f
Alan Yang <alanyang@umich.edu>
2020-12-14 20:53:51 -0500

    SideInv

---
+++ Assets/Scripts/FrameToggler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameToggler : MonoBehaviour
+{
+    public GameObject frame;
+
+    public void toggle()
+    {
+        frame.SetActive(!frame.activeSelf);
+    }
+
+    public void enable()
+    {
+        frame.SetActive(true);
+    }
+
+    public void disable()
+    {
+        frame.SetActive(false);
+    }
+}
